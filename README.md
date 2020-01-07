fb353f7568e261692d82363c19c176ca5a2179a3
050644zf <05644zf@outlook.com>
2020-01-07 19:38:51 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Arknights Story Reader
+
+A simple python program that help you convert Arknights raw story data into other readable format.
+
+## v1.0 CSV Convertor 
+usage:
+```
+csvconvert.py p
+```
+
+Convert arknights story raw data into csv file.
+
+positional arguments:
+  p           The path of raw story file
+
+
+**To-do list: (maybe never)**
+- json convertor
+- link the cg or background image to [Monncell Wiki](http://ak.mooncell.wiki/w/%E5%89%A7%E6%83%85%E8%B5%84%E6%BA%90%E4%B8%80%E8%A7%88) or [Aceship Toolbox](https://aceship.github.io/AN-EN-Tags/akgallery.html)
+- Prettify the output into html or anything looks better.
---
+++ csvconvert.py
@@ -0,0 +1,57 @@
+#This Program convert .txt form story data into .csv
+
+
+import re
+import csv
+import argparse
+
+def reader(rawstorypath):
+    optre=r"^\[Decision\(options=\"(?P<options>.+)\","
+    indre=r"^\[Predicate\(references=\"(?P<index>.+)\""
+    imgre=r"\[(?P<type>.+)\(image=\"(?P<image>.*?)\""
+    namere=r"^\[name=\"(?P<name>.*?)\"\]\s+(?P<text>.+)"
+    with open(rawstorypath,encoding='utf-8') as rawstory:
+        rawstorytext=rawstory.read()
+        rawstorylist=rawstorytext.split('\n')[2:-3]
+        rawlist=[]
+        for line in rawstorylist:
+            if '//' not in line and line!='':
+                if '[' not in line:
+                    line='[name=""]  '+line
+                if '[Dialog]' in line:
+                    line='[name="----"]  ----'
+                if '[Decision' in line:
+                    options=re.match(optre,line).group('options').split(';')
+                    i=1
+                    rawlist.append('[name="--Decision--"]  ----')
+                    for option in options:
+                        rawlist.append('[name="Option_'+str(i)+'"]  '+option)
+                        i=i+1
+                    rawlist.append('[name="--Decision End--"]  ----')
+                    continue
+                if '[Predicate' in line:
+                    index=re.match(indre,line).group('index').replace(';','&')
+                    line='[name="--Branch--"]  >Option_'+index
+                if 'image=' in line:
+                    [imgtype,image]=re.match(imgre,line).group('type','image')
+                    line='[name="--'+imgtype+'--"]  '+image+'.png'
+                if '[name' in line:
+                    rawlist.append(line)
+
+    outputlist=[]
+    for line in rawlist:
+        [name,text]=re.match(namere,line).group('name',"text")
+        outputlist.append([name,text])
+
+    with open(rawstorypath.replace('.txt','.csv'),'w',newline='') as csvfile:
+        writer=csv.writer(csvfile,dialect='excel')
+        writer.writerows(outputlist)
+        
+        
+
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description='Convert arknights story raw data into csv file.')
+    parser.add_argument('rawpath',metavar='p',type=str,help='The path of raw story file')
+    args=parser.parse_args()
+    reader(args.rawpath)
