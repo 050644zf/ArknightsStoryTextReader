f255364f8fd77259faf4dbae8584a980067f5eb5
Nightsky <050644zf@outlook.com>
2024-04-15 23:08:32 +0800

    Update vite.config.js

--- reader/vite.config.js
+++ reader/vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/ArknightsStoryTextReader/',
   build: {
     rollupOptions: {
       input: {
